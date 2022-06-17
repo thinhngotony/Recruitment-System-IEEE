4ca577c8f6cd5f967c3eb2593fbeab46147a29ef
Tony Ngô <thinhngo.tony@gmail.com>
2022-06-17 11:59:12 +0700

    Finish api for send dataset to cloud

--- facial_regconition/Client/02_face_training.py
+++ facial_regconition/Client/02_face_training.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
 
 # Path for face image database
 path = 'dataset/'
@@ -37,6 +42,18 @@ recognizer.train(faces, np.array(ids))
 
 # Save the model into trainer/trainer.yml
 recognizer.write('trainer.yml') 
-
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+print ("\n [INFO] Seding your data. Wait ...")
+folder = '1jx69VcO8uSBKUslgzQAmVKMzNkYm9kl0'
+upload_file_list = ['trainer.yml']
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'parents': [{'id': folder}]})
+	# Read file and set it as the content of this instance.
+	gfile.SetContentFile(upload_file)
+	gfile.Upload() # Upload the file.
+
+print("\n [INFO] Successfully sent your facial recognition data to Server!")
+
+
